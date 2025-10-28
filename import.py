def058af0918d5df26e3aecba71babed19a0943d
Agnar Holst <agnarholst@gmail.com>
2025-10-28 17:54:59 +0100

    Updated import.py to allow for more control in creating table

--- import.py
+++ import.py
@@ -8,12 +8,33 @@ df = pd.read_csv('Letterboxd-Data/watched.csv')
 # Step 2. Data Clean Up
 df.columns = df.columns.str.strip()
 
-# Step 3. Create/connect to a SQLite database
-connection = sqlite3.connect('data.db')
+colmap = {
+    'Date': 'watch_date',
+    'Name': 'title',
+    'Year': 'year',
+    'Letterboxd URI': 'letterboxd_url'
+}
+df = df.rename(columns=colmap)
+df = df[['watch_date', 'title', 'year', 'letterboxd_url']]
 
-# Step 4. Load data file to SQLite 
-# fail;replace;append
-df.to_sql('watched', connection, if_exists='replace')
 
-# Step 5. Close connection
-connection.close()
+# Step 4. Connect and Load data file to SQLite 
+with sqlite3.connect('data.db') as conn:
+    cur = conn.cursor()
+    cur.execute("DROP TABLE IF EXISTS watched;")
+    cur.execute('''
+        CREATE TABLE watched (
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   watch_date TEXT,
+                   title TEXT,
+                   year INTEGER,
+                   letterboxd_url TEXT
+                   );
+               ''')
+    df.to_sql('watched', conn, if_exists='append', index=False)
+
+    total = pd.read_sql_query("SELECT COUNT(*) AS n FROM watched", conn)
+    sample = pd.read_sql_query("SELECT * FROM watched LIMIT 5;", conn)
+    print(total)
+    print(sample)
+
